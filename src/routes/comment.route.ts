7cc1df4d77d9a92c9eaad8b3f6a84b08bd882d59
Brian Pham <nguyenphamswork@gmail.com>
2024-04-16 20:14:58 -0700

    added docs for post endpoint

--- src/routes/comment.route.ts
+++ src/routes/comment.route.ts
@@ -42,16 +42,21 @@ const router = express.Router();
  *             properties:
  *                 id:
  *                     type: string
+ *                     example: clv38h9mx000013uyga53abcde
  *                 comment:
  *                     type: string
+ *                     example: This is a comment
  *                 author:
  *                     type: string
+ *                     example: John Doe
  *                 createdAt:
  *                     type: string
  *                     format: date-time
+ *                     example: 2021-09-01T00:00:00.000Z
  *                 updatedAt:
  *                     type: string
  *                     format: date-time
+ *                     example: 2021-09-01T00:00:00.000Z
  *     securitySchemes:
  *         bearerAuth:
  *             type: http
@@ -59,6 +64,49 @@ const router = express.Router();
  *             bearerFormat: JWT
  */
 router.get("/comments", commentController.getAllComments);
+
+/**
+ * @swagger
+ * /api/v1/comments:
+ *   post:
+ *     summary: Post a new comment
+ *     tags:
+ *       - CommentEndpoints
+ *     description: Send a request to post a new comment to the database
+ *     security:
+ *       - bearerAuth: []
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object   
+ *             properties:
+ *               comment:
+ *                 type: string
+ *               author:
+ *                 type: string 
+ *     parameters:
+ *       - 
+ *         name: authorization
+ *         description: Bearer token
+ *         in: header
+ *         type: string
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Comment'
+ *       401:
+ *         description: Unauthorized
+ *       404:
+ *         description: Not found
+ *       500:
+ *         description: Internal server error
+*/
 router.post("/comments", commentController.postComment);
 
 export default router;
